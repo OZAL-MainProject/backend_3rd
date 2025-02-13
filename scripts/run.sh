8b7226dcce68eff89e5f25a7d2481312cd66bfb3
yerin1562 <yerin1562@gmail.com>
2025-02-13 17:01:29 +0900

    :art: run.sh updated

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,12 +2,7 @@
 set -e
 
 export PYTHONPATH="/app"
-
-if [ "$DJANGO_ENV" = "production" ]; then
-    export DJANGO_SETTINGS_MODULE=config.settings.prod
-else
-    export DJANGO_SETTINGS_MODULE=config.settings.dev
-fi
+export DJANGO_SETTINGS_MODULE=config.settings.prod
 
 poetry run python manage.py collectstatic --noinput
 poetry run python manage.py migrate
