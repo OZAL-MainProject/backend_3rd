f42d29d3230308e578e9b06a19334c7bca0fb43a
yerin1562 <yerin1562@gmail.com>
2025-02-19 13:30:17 +0900

    :sparkles: swagger UI created

--- config/urls.py
+++ config/urls.py
@@ -1,34 +1,22 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
 from users.views import RefreshTokenView
 
-# config url은 뒤에 슬래쉬 붙이고 나머지 url은 앞에 슬래쉬 붙이지마
-
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    # 이 아랫 부분은 우리가 사용하는 app들의 URL들을 넣습니다.
-    path('ozal/', include('users.urls')),
-    path('ozal/trippost/', include('posts.urls')),
-    path("ozal/trippost/", include('likes.urls')),
-    path("api/token/refresh/", RefreshTokenView.as_view(), name="token_refresh"),
-    path("ozal/travel/", include('locations.urls')),
-    path("ozal/users/", include('follows.urls')),
+    path("admin/", admin.site.urls),
 
+    # Swagger API 문서 URL
+    path("ozal/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("ozal/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+    path("ozal/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
+
+    # 기존 API 엔드포인트
+    path("ozal/", include("users.urls")),
+    path("ozal/trippost/", include("posts.urls")),
+    path("ozal/trippost/", include("likes.urls")),
+    path("api/token/refresh/", RefreshTokenView.as_view(), name="token_refresh"),
+    path("ozal/travel/", include("locations.urls")),
+    path("ozal/users/", include("follows.urls")),
 ]
